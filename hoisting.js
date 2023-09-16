946dc1ead8cad914931e7b6749ac6ba0c85453e2
Pre-11 <prernakumari7427@gmail.com>
2023-09-16 20:34:58 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+
+
+console.log("hoisting"+" "+data);
+var data 
+
+sayHii()
+function sayHii(){
+    console.log("Hello Hoisting!");
+}
+
+
+func()
+var func = function sayHello(){
+    console.log("best of luck")
+}
--- promise.js
+++ promise.js
@@ -1,54 +1,30 @@
 
-// function fetchData(){
-//     return new Promise((resolve,reject)=>{
-
-//         setTimeout(()=> {
-//             const data = {name:'John', age:'20'};
-//             const error= false;
-
-//             if(!error){
-//                 resolve(data);
-
-//             } else {
-//                 reject('Error fetching data')
-//             }
-
-
-//         },100)
-    
-//     })
-// }
-
-// fetchData()
-// .then (data=> {
-//     console.log('successful',data)
-// })
-// .catch (error=>{
-//     console.log('error',error);
-// });
-
-
 function fetchData(){
     return new Promise((resolve,reject)=>{
-        const data = {name:'John', age:'20'};
-        const error = false;
 
-        if(!error){
-            resolve(data);
-        }
-        else{
-            reject('Error fetching data');
-        }
+        setTimeout(()=> {
+            const data = {name:'John', age:'20'};
+            const error= false;
+
+            if(!error){
+                resolve(data);
+
+            } else {
+                reject('Error fetching data')
+            }
+
+
+        },100)
+    
     })
 }
 
 fetchData()
-.then (data=>{
+.then (data=> {
     console.log('successful',data)
 })
-
 .catch (error=>{
     console.log('error',error);
-})
+});
 
 
