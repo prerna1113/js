9acb263f3a39bae98fcb64b1b3113676a1beb079
Pre-11 <prernakumari7427@gmail.com>
2023-09-16 19:14:53 +0530

    callback function

---
+++ callback.js
@@ -0,0 +1,26 @@
+
+
+function getUsers(callback){
+
+    setTimeout(()=>{
+        const users = [
+            {
+                name:'John',email:'john@example.com'
+            },
+            {name:'John Smith',email:'john@example'}
+        ];
+        callback(users);
+
+    },1000)
+}
+
+function findUsers(username){
+    
+    getUsers(users=>{
+        const user = users.find(user=> user.name==username)
+        console.log(user)
+    })
+
+}
+
+findUsers('John Smith')
