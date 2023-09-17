93a9e94b4274b442f741a8a35fef9f3dd2dd0858
Pre-11 <prernakumari7427@gmail.com>
2023-09-17 08:27:47 +0530

    added

--- prototype.js
+++ prototype.js
@@ -2,16 +2,13 @@
 function Person(name,age){
     this.name = name;
     this.age = age;
-}
-
-Person.prototype.greet=function(){
-    console.log(`Hello my name is ${this.name} and my age is ${this.age}`)
 
 }
 
-const john = new Person('john',30);
-const jane = new Person('jane',32);
+Person.prototype.greet = function(){
+    console.log(`Hello ${this.name} and ur age is ${this.age}`);
+}
 
-console.log(john.name);
-john.greet();
-jane.greet();
+const dove = new Person('dove', 30);
+
+dove.greet();
