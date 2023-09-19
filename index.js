d6e9dedc8f8883b46f1369dd450c1eae82afd584
Pre-11 <prernakumari7427@gmail.com>
2023-09-19 23:02:26 +0530

    revision

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 
 
 console.log("hello index")
+
+function getUsers(callback) {
+    setTimeout(()=>{
+        const users= [
+            {name: 'John', email: 'john@example.com'},
+            {name:'janet', email: 'janet@example'}
+        ]
+        callback(users)
+    },100)
+
+    
+}
+
+function findUsers(username) {
+    getUsers(users=>{
+        const user = users.find(user=> user.name==username)
+        console.log(user)
+    })
+}
+
+findUsers('John')
