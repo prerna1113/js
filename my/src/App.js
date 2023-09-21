95bd688712b7a05a5fc471ab9cf20470d3b47436
Pre-11 <prernakumari7427@gmail.com>
2023-09-21 11:45:04 +0530

    component added

--- my/src/App.js
+++ my/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from './Component/Component.js'
 
 function App() {
   return (
     <div className="App">
+      <Component />
     
     </div>
   );
---
+++ my/src/Component/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Component(){
+    return (
+        <h1>Component</h1>
+    )
+}
+
+export default Component
