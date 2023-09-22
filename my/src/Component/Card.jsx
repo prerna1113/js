b5bf3290f614211637325b846a3187913daf3301
Pre-11 <prernakumari7427@gmail.com>
2023-09-22 21:56:43 +0530

    added

--- my/src/Component/Card.jsx
+++ my/src/Component/Card.jsx
@@ -1,54 +1,63 @@
 
 
 import React from 'react';
-import Gallery from './Gallery';
 
 function Profile({
+
     heading,
-    imageId,
+    ImageId,
     name,
-    imageSize,
-    awards,
+    size,
     profession,
+    award,
     discovery
-}){
-    return (
+
+})
+
+{
+    return(
         <div>
             <h1>{heading}</h1>
-            <img src={imageId} alt ={name} />
+            <img src={ImageId} alt ={name} style={{width:"100px"}} />
+
             <ul>
-                <li><b>Profession:</b>{profession}</li>
-                <li><b>awards: {awards.length}</b>({awards.join(' ,')})</li>
                 <li>
                     <b>
-                        discovery:
+                        Profession:
                     </b>
+                    {profession}
+                </li>
+                
+                <li>
+                    <b>awards:{award.length}</b>
+                    ({award.join(' ,')})
+                </li>
+
+                <li>
+                    <b>Discovery:</b>
                     {discovery}
                 </li>
             </ul>
-
         </div>
     )
 }
 
- export default function Card(){
+
+export default function Card(){
+
     return(
         <div>
-            <h1>notable scientist</h1>
-            <Profile 
-            heading= "Sihizune"
-            imageId={"https://i.imgur.com/1bX5QH6.jpg"}
-            name= "sihi"
-            profession="physicist and chemist"
-        discovery="polonium (chemical element)"
-        awards={[
-          'Nobel Prize in Physics',
-          'Nobel Prize in Chemistry',
-          'Davy Medal',
-          'Matteucci Medal'
-        ]}
-            />
+            <Profile
+            heading="Naruto"
+            ImageId={"https://wallpapers.com/images/hd/naruto-profile-pictures-sa1tekghfajrr928.jpg"}
+            alt ="naruto"
+            profession="Scientist"
+            award ={[
+                'sasuke','naruto'
+            ]}
+            discovery="orchimaru"
+             />
+             
         </div>
     )
-
 }
