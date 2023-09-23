f6a741804b7be986b41e5b102b8149aaea371a6c
Pre-11 <prernakumari7427@gmail.com>
2023-09-23 20:42:19 +0530

    reusable Event added

--- my/src/Component/Event/Event.jsx
+++ my/src/Component/Event/Event.jsx
@@ -1,6 +1,13 @@
 
 import React from 'react';
 
+function Btn({message,children}){
+  return(
+    <button onClick={()=>alert(message)}>{children}</button>
+  )
+  
+}
+
 function Event(){
 
     const eventHandler =()=>{
@@ -10,6 +17,11 @@ function Event(){
     return(
         <div style={{marginTop:"10%"}}>
             <button  onClick={eventHandler}>Submit</button>
+
+            <button  onClick ={()=>alert('you just clicked me')}
+            >Click me</button>
+
+            <Btn message='playing' children ='play video' />
         </div>
     )
 
